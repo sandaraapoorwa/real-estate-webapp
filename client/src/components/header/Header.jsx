79c7962ad5717ae863741041a4c2c4a7a32e0617
Sandara  Hettiarachchi <apoorwasandara@gmail.com>
2024-12-21 17:50:35 +0530

    updatednavbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { HomeIcon as House } from 'lucide-react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="white" expand="lg" className="shadow-sm py-3">
       <Container>
-        <Navbar.Brand href="#home">Dream Home Realty</Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <House className="text-primary me-2" size={24} />
+          <span className="font-weight-bold">Dream Home Realty</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#properties">Properties</Nav.Link>
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="mx-2">Home</Nav.Link>
+            <Nav.Link href="#properties" className="mx-2">Properties</Nav.Link>
+            <Nav.Link href="#about" className="mx-2">About Us</Nav.Link>
+            <Nav.Link href="#contact" className="mx-2">Contact</Nav.Link>
           </Nav>
+          <Button variant="outline-primary" className="ms-lg-3 mt-2 mt-lg-0">List Your Property</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
